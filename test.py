83c9301fd23c26a8268ec1474dee1327dae49af5
jetxl <mingyun.xl@163.com>
2014-09-12 17:11:23 +0800

    v1.1
    0912

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
 import os
-import sys
-import subprocess
-import PcTest
-import AndroidTest
-import xlrd
+#import sys
+#import subprocess
+#import PcTest
+#import AndroidTest
+#import xlrd
 
-print "Hello, World!";	
+print "Hello, World!"
 def main():
-	xlsx = excel.EasyExcel(os.path.join(os.getcwd(),'结果.xlsx'))
-	xlsx.setCell('Sheet1', 1, 1,'呵呵')
-	xlsx.save()
-	xlsx.close()
-
-if __name__ == '__main__':
+	print "is main!",os.getcwd();	
+	#xlsx = excel.EasyExcel(os.path.join(os.getcwd(),'aa.xlsx'))
+	#xlsx.setCell('Sheet1', 1, 1,'hehe')
+	#xlsx.save()
+	#xlsx.close()
+main()
+#if __name__ == '__main__':
 	#PcTest.main()
-	AndroidTest.main()
+#	AndroidTest.main()
